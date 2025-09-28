fa6026ddabf60f0e341f0b8850f178f3efd4c05a
Jilay Pandya <jilay.pandya@outlook.com>
2025-09-28 13:39:24 +0200

    lib openamp
    Signed-off-by: Jilay Pandya <jilay.pandya@outlook.com>

--- applications/stepper_blocking/src/main.c
+++ applications/stepper_blocking/src/main.c
@@ -13,14 +13,29 @@
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
+static void stepper_print_event_callback(const struct device *dev, enum stepper_event event,
+					 void *user_data)
+{
+	const struct device *dev_callback = user_data;
+
+	LOG_INF("Event %d, %s called for %s, expected for %s\n", event, __func__,
+		dev_callback->name==NULL?"NULL":"soemthong", dev->name);
+}
+
+
 int main(void)
 {
 	const struct device *dev = DEVICE_DT_GET(DT_ALIAS(stepper));
 	stepper_set_microstep_interval(dev, 10000000);
+	stepper_set_event_callback(dev, stepper_print_event_callback, NULL );
 	while (1) {
-		stepper_move_by(dev, 1);
+		int32_t pos;
+		stepper_move_by(dev, 10);
 		k_sleep(K_MSEC(1000));
-		stepper_move_by(dev, -1);
+		stepper_move_by(dev, -10);
+		stepper_stop(dev);
+		//stepper_get_actual_position(dev, &pos);
+		//LOG_INF("pos: %d", pos);
 		k_sleep(K_MSEC(1000));
 	}
 
